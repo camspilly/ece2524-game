58872562dd0e1c0d9b18f0dd16f265435b0585b9
yousbj <yousbj@vt.edu>
2013-12-11 21:51:59 -0500

    Update for Makefile and projectGame.cpp fixing compiliing errors

--- projectGame.cpp
+++ projectGame.cpp
@@ -1,10 +1,11 @@
 #include<iostream>
 #include <ostream>
 #include<sstream>
-#include <curses.h>
 #include <termios.h>
-#include <conio.h>
-#include <stdio.h>
+#include<string>
+#include <fstream>
+//#include <conio.h>
+//#include <stdio.h>
 
 using namespace std;
 char map[50][50];
@@ -30,14 +31,14 @@ int main()
 
 
     position_e currPos;
-    currPos.x= 49;
-    currPos.y= 49;
+    currPos.x_cor= 49;
+    currPos.y_cor= 49;
 
     //Desactivate stdin buffer
     bufferOff();
   
     //Loading map from file
-    loadMap(map1);
+    loadMap("map1");
     updateMap(currPos);
     printMap();
 
@@ -51,49 +52,42 @@ int main()
 
     {
 
-    userInput=getc();
-    mapUpdate();
-    mapPrint();
+    cin >> userInput;
+    updateMap(currPos);
+    printMap();
     switch(userInput)
 
     {
 
-    case 'a': moveLeft();
+    case 'a': moveLeft(currPos);
 
-    case'd': moveRight();
+    case'd': moveRight(currPos);
 
-    case's': moveDown();
+    case's': moveDown(currPos);
 
-    case'w': moveUp();
-
-    }
+    case'w': moveUp(currPos);
 
     }
 
+    }
+     return 0;
 }
 
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    return 0;
-}
-void loadMap(string map)
+   
+
+
+
+
+
+/////////////////////////////////////////////
+
+void loadMap(string fileName)
 {
 
-    ifstream inputFile(map+"txt");
+  ifstream inputFile(fileName.c_str());
     int i,j;
-    for (i=0;i<5-;i++)
+    for (i=0;i<5;i++)
       {
 	for (j=0; j<50; j++)
 	  {
@@ -125,26 +119,26 @@ void bufferOff()
 {  
   //Desactivating standard input buffer
   struct termios t;
-  tcgetattr(STDIN_FILENO, &t);
+  tcgetattr(0, &t);
   t.c_lflag &= ~ICANON;
-  tcsetattr(STDIN_FILENO, TSCANOW, &t);
+  tcsetattr(0, TCSANOW, &t);
 }
 
 
 
 void moveLeft(position_e currPos)
 {
-  currPos.x_cor=(map[currPos.x -1][currPos.y]=="X")? currPos.x_cor: currPos.x_cor--;
+  currPos.x_cor=(map[currPos.x_cor -1][currPos.y_cor]=='X')? currPos.x_cor: currPos.x_cor--;
 }
 void moveRight(position_e currPos)
 {
-  currPos.x_cor=(map[currPos.x +1][currPos.y]=="X")? currPos.x_cor: currPos.x_cor++;
+  currPos.x_cor=(map[currPos.x_cor +1][currPos.y_cor]=='X')? currPos.x_cor: currPos.x_cor++;
 }
 void moveUp(position_e currPos)
 {
-  currPos.y_cor=(map[currPos.x][currPos.y-1]=="X")? currPos.y_cor: currPos.y_cor--;
+  currPos.y_cor=(map[currPos.x_cor][currPos.y_cor -1]=='X')? currPos.y_cor: currPos.y_cor--;
 }
 void moveDown(position_e currPos)
 {
- currPos.y_cor=(map[currPos.x][currPos.y+1]=="X")? currPos.y_cor: currPos.y_cor++;
+ currPos.y_cor=(map[currPos.x_cor][currPos.y_cor +1]=='X')? currPos.y_cor: currPos.y_cor++;
 }
